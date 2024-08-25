f70f6adf666e63d067a26c7c19740577d6ee8480
arungeorgesaji <arungeorgesaji@gmail.com>
2024-08-25 22:27:03 +0530

    Made it so gtk builder xml is used for making the ui

--- src/build_main.rs
+++ src/build_main.rs
@@ -1,47 +1,36 @@
 use gtk4::prelude::*;
-use super::gtk_env::GtkEnv;
-use gtk4::{Application, ApplicationWindow, Notebook, Box, Label, Orientation, Align};
+use gtk4::{Application, ApplicationWindow, Builder, Label, Notebook};
 
 pub fn build_app() -> Application {
-    let gtk_env = GtkEnv::init();
+    let gtk_env = super::gtk_env::GtkEnv::init();
 
     let app = Application::builder()
         .application_id(&gtk_env.application_id)
         .build();
 
-    app.connect_activate(move | app | {
-        let window = build_window(&gtk_env, app);
+    app.connect_activate(move |app| {
+        let window = build_window(app);
         window.present();
     });
 
-    app 
+    app
 }
 
-fn build_window(gtk_env: &GtkEnv, app: &Application) -> ApplicationWindow {
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title(&gtk_env.title)
-        .default_width(gtk_env.default_width)
-        .default_height(gtk_env.default_height)
-        .build();
+fn build_window(app: &Application) -> ApplicationWindow {
+    let builder = Builder::from_file("src/ui/interface.ui");
 
-    let notebook = Notebook::new();
+    let window: ApplicationWindow = builder.object("main_window").expect("Failed to get main_window");
+    window.set_application(Some(app));
 
-    let page1 = Box::new(Orientation::Vertical, 0);
-    let label1 = Label::new(Some("This is the first tab"));
-    page1.append(&label1);
-    notebook.append_page(&page1, Some(&Label::new(Some("Tab 1"))));
+    let notebook: Notebook = builder.object("notebook").expect("Failed to get notebook");
+    let page1: gtk4::Widget = builder.object("page1").expect("Failed to get page1");
+    let page2: gtk4::Widget = builder.object("page2").expect("Failed to get page2");
 
-    let page2 = Box::new(Orientation::Vertical, 0);
-    let label2 = Label::new(Some("This is the second tab"));
-    page2.append(&label2);
-    notebook.append_page(&page2, Some(&Label::new(Some("Tab 2"))));
+    let label1 = Label::new(Some("Containers"));
+    let label2 = Label::new(Some("Virtual Machines"));
 
-    let container = Box::new(Orientation::Vertical, 0);
-    container.set_halign(Align::Center);
-
-    container.append(&notebook);
-    window.set_child(Some(&container));
+    notebook.set_tab_label(&page1, Some(&label1));
+    notebook.set_tab_label(&page2, Some(&label2));
 
     window
 }
