ebf92012f74b3812d340d6b6c55de6a1f752e534
arungeorgesaji <arungeorgesaji@gmail.com>
2024-08-26 09:02:53 +0530

    Made css work,it only works when css class is added from the rust itself and not the xml also added some basic styling

--- src/build_main.rs
+++ src/build_main.rs
@@ -39,6 +39,7 @@ fn build_window(app: &Application) -> ApplicationWindow {
     let window: ApplicationWindow = builder.object("main_window").expect("Failed to get main_window");
     window.set_application(Some(app));
 
+    let main_box: gtk4::Box = builder.object("main_box").expect("failed to get main_box");
     let notebook: Notebook = builder.object("notebook").expect("Failed to get notebook");
     let page1: gtk4::Widget = builder.object("page1").expect("Failed to get page1");
     let page2: gtk4::Widget = builder.object("page2").expect("Failed to get page2");
@@ -46,8 +47,25 @@ fn build_window(app: &Application) -> ApplicationWindow {
     let label1 = Label::new(Some("Containers"));
     let label2 = Label::new(Some("Virtual Machines"));
 
+    main_box.add_css_class("main_box");
+    notebook.add_css_class("notebook");
+    page1.add_css_class("page1");
+    page2.add_css_class("page2");
+    label1.add_css_class("label1");
+    label2.add_css_class("label2");
+    
     notebook.set_tab_label(&page1, Some(&label1));
     notebook.set_tab_label(&page2, Some(&label2));
+
+    label1.set_hexpand(true);
+    label1.set_vexpand(true);
+    label1.set_halign(gtk4::Align::Fill);
+    label1.set_valign(gtk4::Align::Center);
+
+    label2.set_hexpand(true);
+    label2.set_vexpand(true);
+    label2.set_halign(gtk4::Align::Fill);
+    label2.set_valign(gtk4::Align::Center);
 
     window
 }
