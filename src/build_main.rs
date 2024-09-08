e39cebb8f657f3653ca260b6193cbc79d0e699e7
arungeorgesaji <arungeorgesaji@gmail.com>
2024-09-08 21:44:24 +0530

    Splitted the UI files into 3(main_window,page1,page2)

--- src/build_main.rs
+++ src/build_main.rs
@@ -5,27 +5,21 @@ use gio::File;
 
 pub fn build_app() -> Application {
     let gtk_env = super::gtk_env::GtkEnv::init();
-
     let app = Application::builder()
         .application_id(&gtk_env.application_id)
         .build();
-
     app.connect_startup(|_| load_css());
-
     app.connect_activate(move |app| {
         let window = build_window(app);
         window.present();
     });
-
     app
 }
 
 fn load_css() {
     let provider = CssProvider::new();
-    let file = File::for_path("src/ui/style.css");
-
+    let file = File::for_path("style.css");
     provider.load_from_file(&file); 
-
     gtk4::style_context_add_provider_for_display(
         &Display::default().expect("Could not connect to a display."),
         &provider,
@@ -34,18 +28,24 @@ fn load_css() {
 }
 
 fn build_window(app: &Application) -> ApplicationWindow {
-    let builder = Builder::from_file("src/ui/interface.ui");
-
-    let window: ApplicationWindow = builder.object("main_window").expect("Failed to get main_window");
+    let main_window_builder = Builder::from_file("main_window.ui");
+    let page1_builder = Builder::from_file("page1.ui");
+    let page2_builder = Builder::from_file("page2.ui");
+    
+    let window: ApplicationWindow = main_window_builder.object("main_window").expect("Failed to get main_window");
     window.set_application(Some(app));
-
-    let main_box: gtk4::Box = builder.object("main_box").expect("failed to get main_box");
-    let notebook: Notebook = builder.object("notebook").expect("Failed to get notebook");
-    let page1: gtk4::Widget = builder.object("page1").expect("Failed to get page1");
-    let page2: gtk4::Widget = builder.object("page2").expect("Failed to get page2");
+    
+    let main_box: gtk4::Box = main_window_builder.object("main_box").expect("failed to get main_box");
+    let notebook: Notebook = main_window_builder.object("notebook").expect("Failed to get notebook");
+    
+    let page1: gtk4::Widget = page1_builder.object("page1").expect("Failed to get page1");
+    let page2: gtk4::Widget = page2_builder.object("page2").expect("Failed to get page2");
 
     let label1 = Label::new(Some("Containers"));
     let label2 = Label::new(Some("Virtual Machines"));
+    
+    notebook.append_page(&page1, Some(&label1));
+    notebook.append_page(&page2, Some(&label2));
 
     main_box.add_css_class("main_box");
     notebook.add_css_class("notebook");
@@ -54,19 +54,14 @@ fn build_window(app: &Application) -> ApplicationWindow {
     label1.add_css_class("label1");
     label2.add_css_class("label2");
     
-    notebook.set_tab_label(&page1, Some(&label1));
-    notebook.set_tab_label(&page2, Some(&label2));
-
     label1.set_hexpand(true);
     label1.set_vexpand(true);
     label1.set_halign(gtk4::Align::Fill);
     label1.set_valign(gtk4::Align::Center);
-
     label2.set_hexpand(true);
     label2.set_vexpand(true);
     label2.set_halign(gtk4::Align::Fill);
     label2.set_valign(gtk4::Align::Center);
-
+    
     window
 }
-
