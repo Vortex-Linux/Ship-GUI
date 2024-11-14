6a6a4bb18f02c762e8aa06da9e9128c533066c5a
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-14 19:47:48 +0530

    Added active propety for the nav buttons and added styles for it

--- src/nav.cpp
+++ src/nav.cpp
@@ -1,14 +1,28 @@
 #include "nav.h"
+#include <QStyle>
 #include <QPushButton>
 
 Nav::Nav(QWidget *parent) : QWidget(parent), ui(new Ui::Nav) {
     ui->setupUi(this);
 
+    ui->container_button->setProperty("active", true);
+    ui->vm_button->setProperty("active", false);
+    ui->container_button->style()->polish(ui->container_button);
+    ui->vm_button->style()->polish(ui->vm_button);
+
     connect(ui->container_button, &QPushButton::clicked, [=]() {
+        ui->container_button->setProperty("active", true);
+        ui->vm_button->setProperty("active", false);
+        ui->container_button->style()->polish(ui->container_button);
+        ui->vm_button->style()->polish(ui->vm_button);
         emit buttonClicked("containers");
     });
 
     connect(ui->vm_button, &QPushButton::clicked, [=]() {
+        ui->container_button->setProperty("active", false);
+        ui->vm_button->setProperty("active", true);
+        ui->container_button->style()->polish(ui->container_button);
+        ui->vm_button->style()->polish(ui->vm_button);
         emit buttonClicked("virtual_machines");
     });
 }
