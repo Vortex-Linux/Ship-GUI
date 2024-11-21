5e4e92219bed7d070c78845ce9f9cf70d39e1ee2
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-21 22:07:17 +0530

    Added headers for all vm operations planned to be available from gui for now

--- include/vm_operations.h
+++ include/vm_operations.h
@@ -7,6 +7,14 @@
 std::vector<std::string> list_vm_names();
 std::vector<std::string> list_vm_status();
 void start_vm(std::string vm_name);
+void restartVM(std::string vm_name);
+void deleteVM(std::string vm_name);
+void viewVM(std::string vm_name);
+void pauseVM(std::string vm_name);
+void resumeVM(std::string vm_name);
+void saveVM(std::string vm_name);
+void shutdownVM(std::string vm_name);
+void sendVM(std::string vm_name);
 
 #endif 
 
