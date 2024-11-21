d1248ca2e55e89a6007100d39cdb7339f2410aab
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-21 22:09:54 +0530

    Added headers for all container operations planned to be available from gui for now

--- include/container_operations.h
+++ include/container_operations.h
@@ -6,6 +6,11 @@
 
 std::vector<std::string> list_container_names();
 std::vector<std::string> list_container_status();
+void deleteContainer(std::string container_name);
+void viewContainer(std::string container_name);
+void upgradeContainer(std::string container_name);
+void stopContainer(std::string container_name);
+void sendContainer(std::string container_name);
 
 #endif 
 
